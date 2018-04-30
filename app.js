d7dd120df29d82dbb02033e9c96f4cc3af7e7b49
Laura Bochenek <laura.bochenek@gmail.com>
2018-04-30 14:20:09 -0400

    boilerplate - v4 bot framework

---
+++ app.js
@@ -0,0 +1,25 @@
+const botbuilder = require('botbuilder');
+const restify = require('restify');
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter (it's ok for MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD to be blank for now)  
+const adapter = new botbuilder.BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+});
+
+
+// Listen for incoming requests 
+server.post('/api/messages', (req, res) => {
+    // Route received request to adapter for processing
+    adapter.processActivity(req, res, async (context) => {
+        if (context.activity.type === 'message') {
+            await context.sendActivity(`Hello World!`);
+        }
+    });
+});
---
+++ notes.md
@@ -0,0 +1,21 @@
+# notes
+
+## azure portal
+
+### Types of Azure portal bots
+
+#### Web App Bot
+
+Deployed to Azure App Service Web App
+
+[App Service](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction) plan - predefined capacity with predictable costs and scaling
+
+#### Functions Bot
+
+Deployed to Azure Functions App
+
+[Consumption plan(https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction)] - runs on Azure Functions and uses the pay-per-run Azure Functions pricing
+
+#### [Bot Channels](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-quickstart-registration)
+
+If have bot hosted elsewhere and want to use Bot Service to connect the bot to other channels
