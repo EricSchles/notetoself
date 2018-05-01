be2357e3c96b7a3d52ad27170aad0c672a6fe00f
Laura Bochenek <laura.bochenek@gmail.com>
2018-05-01 13:47:39 -0400

    basic conversation flow

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const botbuilder = require('botbuilder');
 const restify = require('restify');
 require('dotenv').config();
+const DBHelper = require('./helpers/database');
+const dbhelper = new DBHelper();
 
 // Create server
 let server = restify.createServer();
@@ -14,25 +16,86 @@ const adapter = new botbuilder.BotFrameworkAdapter({
     appPassword: process.env.MICROSOFT_APP_PASSWORD 
 });
 
+// Add conversation state middleware
+const conversationState = new botbuilder.ConversationState(new botbuilder.MemoryStorage()); 
+adapter.use(conversationState);
 
 // Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
     // Route received request to adapter for processing
     adapter.processActivity(req, res, async (context) => {
         if (context.activity.type === 'message') {
-            const message = processMessage(context.activity.text, context.activity.channelId);
-            await context.sendActivity(message);
+            const message = processMessage(context.activity.text, context.activity.channelId, context.activity.conversation.conversationType);
+            const state = conversationState.get(context);
+            if (!state.prompt) {
+                if (message === 'save') {
+                    state.prompt = 'type';
+                    const saveContentMessageCard = botbuilder.MessageFactory.attachment(botbuilder.CardFactory.heroCard('What type of content do you want to save?', null, [
+                        {
+                            "type": "imBack",
+                            "title": "Link",
+                            "value": "link",
+                            "displaytext": "link"
+                        },
+                        {
+                            "type": "imBack",
+                            "title": "Text",
+                            "value": "text",
+                            "displaytext": "text"
+                        },
+                        {
+                            "type": "imBack",
+                            "title": "Code",
+                            "value": "code",
+                            "displaytext": "code"
+                        },
+                        {
+                            "type": "imBack",
+                            "title": "Other",
+                            "value": "other",
+                            "displaytext": "other"
+                        }
+                    ]));
+                    await context.sendActivity(saveContentMessageCard);
+                } else {
+                    await context.sendActivity(message);
+                }
+            } else {
+                switch(state.prompt) {
+                    case 'type':
+                        //TODO manage content
+                        state.prompt = 'content';
+                        await context.sendActivity('Great! Now please send me the content you want to save');
+                        break;
+                    case 'content':
+                        //TODO manage content
+                        state.prompt = 'tags';
+                        await context.sendActivity('Thank you! What do you want to tag this as? Please enter a comma-deliminated list');
+                        break;
+                    case 'tags':
+                        //TODO manage content
+                        state.prompt = undefined;
+                        await context.sendActivity('Alright~ I\'ll save it forever and ever~');
+                        //TODO: add undo
+                        break;
+                    default:
+                        await context.sendActivity('I don\'t know what\'s going on!');
+                        break;
+                }
+            }
         } else if (context.activity.type === 'conversationUpdate') {
             if (context.activity.membersAdded &&
                 context.activity.membersAdded[0].id.includes(process.env.MICROSOFT_APP_ID)) {
                 await context.sendActivity("Hello! I'm notetoself, the bot that will store and organize stuff you send to yourself!");
             }
+        } else {
+            await context.sendActivity("I don't understand what you're saying!");
         }
     });
 });
 
-const processMessage = (message, channel) => {
-    if (channel === "msteams") {
+const processMessage = (message, channel, convoType) => {
+    if (channel === "msteams" && convoType === 'channel') {
         //+9 for the <at></at> tags
         message = message.slice(process.env.BOT_NAME.length+9, message.length);
     }
---
+++ helpers/database.js
@@ -0,0 +1,7 @@
+class DatabaseHelper {
+    saveNote(note) {
+        //TODO: set up database / saving note mechanism
+    }
+}
+
+module.exports = DatabaseHelper;
